d1b1bfb83213357ee85f64a0fab0c734c2ea33d9
Fuocc <fedydev96@gmail.com>
2025-08-12 01:49:28 +0700

    add file env and updated file server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const nodemailer = require('nodemailer');
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const nodemailer = require("nodemailer");
 
 const app = express();
 const PORT = 3001;
@@ -8,39 +10,39 @@ const PORT = 3001;
 app.use(cors());
 app.use(express.json());
 
-app.post('/api/contact', async (req, res) => {
-  const { firstName, lastName, email, phone, licenses, message } = req.body;
+app.post("/api/contact", async (req, res) => {
+    const { firstName, lastName, email, phone, licenses, message } = req.body;
 
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'your.email@gmail.com', // Thay bằng email thật
-      pass: 'your-app-password', // Sử dụng app password, không dùng mật khẩu gmail chính
-    },
-  });
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+        },
+    });
 
-  const mailOptions = {
-    from: email,
-    to: 'your.email@gmail.com',
-    subject: `Yêu cầu liên hệ từ ${firstName} ${lastName}`,
-    text: `
+    const mailOptions = {
+        from: email,
+        to: "dangthienphuoc24@gmail.com",
+        subject: `Yêu cầu liên hệ từ ${firstName} ${lastName}`,
+        text: `
       Họ tên: ${lastName} ${firstName}
       Email: ${email}
       SĐT: ${phone}
       Số lượng license: ${licenses}
       Lời nhắn: ${message}
     `,
-  };
+    };
 
-  try {
-    await transporter.sendMail(mailOptions);
-    res.status(200).send({ message: 'Email đã được gửi thành công!' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send({ message: 'Đã xảy ra lỗi khi gửi email.' });
-  }
+    try {
+        await transporter.sendMail(mailOptions);
+        res.status(200).send({ message: "Email đã được gửi thành công!" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: "Đã xảy ra lỗi khi gửi email." });
+    }
 });
 
 app.listen(PORT, () => {
-  console.log(`Server đang chạy tại http://localhost:${PORT}`);
+    console.log(`Server đang chạy tại http://localhost:${PORT}`);
 });
