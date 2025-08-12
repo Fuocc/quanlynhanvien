49e746bbe1e0686d91d87c87f7dc293c1f199447
Fuocc <150522816+Fuocc@users.noreply.github.com>
2025-08-12 23:26:48 +0700

    Update Button.jsx

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -2,18 +2,18 @@ import { Link } from "react-router";
 
 export default function Button({ children, isStroke, ...props }) {
     return isStroke ? (
-        <a
+        <Link
             href="/lien-he"
             className={`bg-transparent color-black font-medium border border-[#1f2937] rounded-[5px] px-7 py-3.5 transtion-all duration-250 ease-out hover:bg-[#3B82F6] hover:text-white hover:border-transparent ${props.className} max-md:text-lg`}
         >
             {children}
-        </a>
+        </Link>
     ) : (
-        <a
+        <Link
             href="/lien-he"
             className={`bg-[#3B82F6] text-white font-medium rounded-[5px] px-7 py-3.5 transition-all duration-250 ease-out hover:bg-[#1E82DC] ${props.className} max-md:text-lg`}
         >
             {children}
-        </a>
+        </Link>
     );
 }
